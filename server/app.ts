697e1af65869db682f1a39374d62346e5b276699
neintanukiplus <>
2022-12-22 19:06:01 +0800

    implemented routes and other configs

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,32 @@
 import { Server } from "colyseus"
-const port = parseInt(process.env.PORT, 10) || 3000
+import { WebSocketTransport } from "@colyseus/ws-transport"
+import express from 'express'
+import mongoose from "mongoose"
+import http from 'http'
+import * as dotenv from 'dotenv'
 
-const gameServer = new Server()
-gameServer.listen(port)
-console.log(`[GameServer] Listening on Port: ${port}`)
+import authRoutes from './controllers/auth'
+
+dotenv.config()
+
+const PORT: number = parseInt(process.env.PORT ?? '') ?? 3000
+const DB_URI: string = process.env.DB_URI ?? ''
+
+const app = express()
+const server = http.createServer(app)
+const gameServer = new Server({
+    transport: new WebSocketTransport({
+        server
+    })
+})
+
+app.use(express.json())
+
+app.use('/', authRoutes)
+
+mongoose.connect(DB_URI, err => {
+    if (!err) {
+        gameServer.listen(PORT)
+        console.log(`[GameServer] Listening on Port: ${PORT}`)
+    }
+})
