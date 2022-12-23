3f7da0db0a4da274353666e3d25c2f6d372e4dd5
neintanukiplus <>
2022-12-23 20:22:15 +0800

    implemented loaders

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,11 +1,21 @@
+import { MouseEvent } from "react"
 import { IDefaultProps } from "../types/Components"
+
+import { ThreeDots } from 'react-loader-spinner'
 
 import style from './styles/Button.module.css'
 
-export default function Button({ children }: IDefaultProps) {
+interface Props extends IDefaultProps {
+    evt?: (event: MouseEvent<HTMLButtonElement>) => void,
+    isDisabled?: boolean
+}
+
+export default function Button({ children, evt, isDisabled = false }: Props) {
     return (
         <div className={style.btn}>
-            <button>{ children }</button>
+            <button onClick={evt} disabled={isDisabled}>
+                { !isDisabled ? children : <ThreeDots width={15} height={15} color={"#fff"} /> }
+            </button>
         </div>
     )
 }
