5624efe17bd8e0309bc486b664498cf20697550e
neintanukiplus <>
2022-12-23 20:09:12 +0800

    added cors

--- server/app.ts
+++ server/app.ts
@@ -4,6 +4,7 @@ import express from 'express'
 import mongoose from "mongoose"
 import http from 'http'
 import * as dotenv from 'dotenv'
+import cors from 'cors'
 
 import authRoutes from './controllers/auth'
 
@@ -20,6 +21,7 @@ const gameServer = new Server({
     })
 })
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/', authRoutes)
