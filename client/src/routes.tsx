0bb3f7ca9bd0c1b58d814145a8688a8879e158a1
neintanukiplus <>
2022-12-25 18:05:06 +0800

    added dashboard route

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
+import Dashboard from "./pages/Dashboard";
 
 export default createBrowserRouter([
     {
         path: '/',
         element: <Homepage />
     },
+    {
+        path: "/dashboard",
+        element: <Dashboard />
+    }
 ])
