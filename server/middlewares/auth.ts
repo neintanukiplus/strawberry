1424115276ad5898dba9e2e1be72c95d086987c6
neintanukiplus <>
2022-12-27 17:41:13 +0800

    added auth middleware

---
+++ server/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import {Request, Response, NextFunction} from "express";
+import { verifyToken } from "../utils/auth";
+
+import Developer from "../models/Developer";
+import { IJWTToken } from "../types/auth";
+
+export function isLoggedIn(req: Request, res: Response, next: NextFunction) {
+    const { token } = req.cookies
+
+    if (token) {
+        verifyToken(token, async (decoded: IJWTToken, err: Error) => {
+            if (err) {
+                res.status(400).end()
+            } else {
+                const { id } = decoded
+
+                try {
+                    const dev = await Developer.findById(id)
+
+                    if (dev) {
+                        next()
+                    } else {
+                        res.status(401).end()
+                    }
+                } catch (error) {
+                    res.status(401).end()
+                }
+            }
+        })
+    } else {
+        res.status(401).end()
+    }
+}
