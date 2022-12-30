4628111779e859e4f979dce78ba8163478f8de49
neintanukiplus <>
2022-12-30 16:04:00 +0800

    passed server-side data to client

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,24 +1,30 @@
-import { useEffect, useState } from "react";
+import { AxiosResponse } from "axios";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import isDevLoggedIn from "../services/isDevLoggedIn";
 import { IDefaultProps } from "../types/Components";
+import { ServerDataContext } from "../contexts/ServerDataContext";
 
 export default function PrivateRoute({ children }: IDefaultProps) {
     const navigate = useNavigate()
+    const [data, setData] = useState({})
     const [isLoggedIn, setIsLoggedIn] = useState(false)
 
     useEffect(() => {
         (async () => {
-            const res = await isDevLoggedIn()
-            setIsLoggedIn(res)
+            const res: (AxiosResponse|null) = await isDevLoggedIn()
 
             if (!res) {
+                setIsLoggedIn(false)
                 navigate("/")
+            } else {
+                setData(res?.data.dev)
+                setIsLoggedIn(true)
             }
         })()
     }, [])
 
     return (
-        <>{ isLoggedIn && children }</>
+        <>{ isLoggedIn && <ServerDataContext.Provider value={data}>{children}</ServerDataContext.Provider> }</>
     )
 }
