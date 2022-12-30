226c83e1f7e3e205e239b920a373244f84802b9f
neintanukiplus <>
2022-12-30 16:05:51 +0800

    displayed dev name to nav

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -9,13 +9,17 @@ import Modal from '../components/Modal'
 import CreateRoomModal from '../components/CreateRoomModal'
 import RemoveRoomModal from '../components/RemoveRoomModal'
 import Room from '../components/Room'
+import { useContext } from 'react'
+import { ServerDataContext } from '../contexts/ServerDataContext'
+import { IDevResponse } from '../types/Response'
 
 export default function Dashboard() {
+    const serverData: IDevResponse = useContext(ServerDataContext)
 
     return (
         <main className={style.dashboard}>
             <div className={style["inner-container"]}>
-                <nav><ProfilePicture value='Romero' size={30} />Romero</nav>
+                <nav><ProfilePicture value={serverData.name || ""} size={30} />{serverData?.name}</nav>
                 <header>
                     <div className={style.left}>
                         <h1 className='title'>Dashboard</h1>
@@ -31,6 +35,7 @@ export default function Dashboard() {
                     <Room />
                     <Room />
                 </section>
+
             </div>
 
             {/* <CreateRoomModal isShowing={true} /> */}
---
+++ client/src/pages/Join.tsx
@@ -0,0 +1,19 @@
+export default function Join() {
+    return (
+        <main className="join">
+            <div className="media-playback">
+                video player <br />
+                audio btn and camera btn <br />
+                audio volume <br />
+                device picker
+            </div>
+ 
+            <div className="user-info">
+                room id <br />
+                name <br />
+                notice <br />
+                join room
+            </div>
+        </main>
+    )
+}
